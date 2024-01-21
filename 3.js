030371adb77c99b71440bbd92218c10b3202b7f3
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2024-01-21 12:10:24 +0530

    minor changes

--- 3.js
+++ 3.js
@@ -10,11 +10,12 @@
     5. +
     6. -
 2.Compare
-    1. ==
-    2. >
-    3. >=
-    4. <
-    5. <=
+    1. ===
+    2. ==
+    3. >
+    4. >=
+    5. <
+    6. <=
 
 3.Logical
     1. !  = not = invert the value
@@ -24,11 +25,19 @@
 4.Assignment
     =
 */ 
+
+//Examples:
+
 console.log(7%2)
 console.log(4*(3**2 -1) + 5/2 -1)//first inside the () by starting getting the exponential(3**2 = 9) to substract then that value multiply by 4 and then 5 devided by 2  then add them and substract1
 //          4*8 +2.5 -1 = 32 + 2.5 - 1 = 33.5
 
 console.log(10 == 10)
+console.log(10 == "10")
+console.log(10 != "10")
+console.log(10 === 10)
+console.log(10 === "10")
+console.log(10 !== "10")
 console.log(5 == (4+1))
 console.log(!(10 == 10))
 console.log((10 == 10) && (4 > 3))
---
+++ 4.js
@@ -0,0 +1,16 @@
+// strings
+/*
+These are some examples
+
+""
+''
+'hello world'
+"hello world"
+
+below one is wrong because it has some confusing notation when string start and end
+'I'm trx'
+so we use below notation for clarity and correctness
+"I'm trx" 
+
+*/
+
