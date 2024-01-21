4e7fe38017b25e7b4f3bb839e2c8f69141455b1c
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2024-01-21 09:50:47 +0530

    ops

---
+++ 3.js
@@ -0,0 +1,37 @@
+//Operators
+//I show each operations type and inside them I wrote in their peiority high to low
+/*
+1.Arithmetic
+    1.()
+    2.* 
+    3. /
+    4. +
+    5. -
+2.Compare
+    1. ==
+    2. >
+    3. >=
+    4. <
+    5. <=
+
+3.Logical
+    1. !  = not = invert the value
+    2. && = and = all must be true to be true
+    3. || = or  = all must be false to be false
+
+4.Assignment
+    =
+*/ 
+console.log(4*(3*2 -1) + 5/2 -1)//first inside the () by starting multiplying to substract then that value multiply by 4 and then 5 devided by 2  then add them and substract1
+//          4*5 +2.5 -1 = 20 + 2.5 - 1 = 21.5
+
+console.log(10 == 10)
+console.log(5 == (4+1))
+console.log(!(10 == 10))
+console.log((10 == 10) && (4 > 3))
+console.log((10 == 10) && (4 > 5))
+console.log((10 == 10) || (4 > 3))
+console.log((10 == 11) || (4 > 3))
+
+
+
