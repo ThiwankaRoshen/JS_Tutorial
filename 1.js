4888251d3e45da68f28a34146fee3627ef25fff6
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2024-01-21 09:11:06 +0530

    1 is completed

---
+++ 1.js
@@ -0,0 +1,24 @@
+// single line comment
+
+/*
+m
+u
+l
+t
+i 
+ 
+line
+comment
+*/
+
+// We use them to
+/*explain codes
+or turn off some codes if we don't want like,
+console.log('hello')
+line 17 code don't run because it is commented out.*/
+
+console.log('hello world') //we use it to print messages
+/*we can style the message by adding %c in the message and add another arguement 
+(console.log(argument1, argument2)   argument1 is the printing statement argument2 is for styling ) */ 
+console.log('%chello world', "color: red; font-size: 35px;")//you can type this in your browser devtools and see output
+ 
