491ade7ffd16f4afa2f69c7b0a833a30e4f37fac
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2024-01-21 09:31:23 +0530

    variables intro

---
+++ 2.js
@@ -0,0 +1,23 @@
+//variables which are like containers that store data and we give label for that container
+
+/*we get a input as user name and surely we need to remember through out the code. if not we have to ask evertime
+when we want. that is why we use variables.*/
+
+//declaration of a variable
+var variable_name = "TRX"
+
+//if we want to console it
+console.log(variable_name)
+
+//if we want to add something more with it
+//1st method
+console.log('hey', variable_name)
+//2nd method
+console.log('hey' + variable_name)
+//what was the difference between them?
+//also we can many words in this way not only two words.
+
+//also we can change the existing values in them
+variable_name = "Rosh"
+//lets check what happend
+console.log(variable_name)
